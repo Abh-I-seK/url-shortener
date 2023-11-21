56b7368aea0a4dcece2a66409e44166ab8b8f2cd
Abh-I-sek <abhisek211004@gmail.com>
2023-11-21 20:42:42 +0530

    fix

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true
+  reactStrictMode: true,
+  headers: {
+    'Access-Control-Allow-Origin': ['https://url-shortener-ochre-tau.vercel.app/']
+  },
 }
 
 module.exports = nextConfig;
