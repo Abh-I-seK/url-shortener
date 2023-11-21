081536fadce17ec7d3cd8e318a6f36a0e2c8b292
Abh-I-sek <abhisek211004@gmail.com>
2023-11-21 20:00:08 +0530

    Fixed CORS

--- next.config.js
+++ next.config.js
@@ -3,4 +3,6 @@ const nextConfig = {
   reactStrictMode: true,
 }
 
-module.exports = nextConfig
+module.exports = {headers: {
+  'Access-Control-Allow-Origin': ['https://url-shortener-chi-eight.vercel.app/','https://url-shortener-9rnjhqmgc-abhisek211004-gmailcom.vercel.app/' ,'https://url-shortener-git-main-abhisek211004-gmailcom.vercel.app/'],
+},nextConfig}
