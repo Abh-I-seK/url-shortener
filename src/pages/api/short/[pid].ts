87e737f7835941787dbe0076c4b5243d0ff7d10d
Abh-I-sek <abhisek211004@gmail.com>
2023-11-21 21:59:11 +0530

    fix

--- src/pages/api/short/[pid].ts
+++ src/pages/api/short/[pid].ts
@@ -7,10 +7,12 @@ export default async function handler(
   res: NextApiResponse
 ) {
     await connectMongoose(process.env.DB_LINK);
+    console.log("hmm");
     const {pid}=req.query;
     
     const exist = await Url.find({nos:pid})
     const h=exist[0].url;
+    console.log("hmm");
     console.log(h);
     res.json({main:h});
     res.redirect(h);
