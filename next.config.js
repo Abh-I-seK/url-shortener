54dd60b2b1a3c8c87563d75cdf723f8c8ba3a631
Abh-I-sek <abhisek211004@gmail.com>
2023-11-21 20:29:28 +0530

    Fix

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,headers: {
-    'Access-Control-Allow-Origin': ['https://url-shortener-chi-eight.vercel.app/','https://url-shortener-9rnjhqmgc-abhisek211004-gmailcom.vercel.app/' ,'https://url-shortener-git-main-abhisek211004-gmailcom.vercel.app/'],
-  }
+  reactStrictMode: true
 }
 
 module.exports = nextConfig;
